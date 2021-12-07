7de38be6175a291655e16b48a7e3fd34266006c8
Hide <85694642+HideYorozuya@users.noreply.github.com>
2021-12-07 20:08:34 +0530

    Add files via upload

---
+++ CsereTests/CsereTests.swift
@@ -0,0 +1,33 @@
+//
+//  CsereTests.swift
+//  CsereTests
+//
+//  Created by Athul Sasikumar on 23/08/1943 Saka.
+//
+
+import XCTest
+@testable import Csere
+
+class CsereTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Resources/StorageManager.swift
@@ -0,0 +1,64 @@
+//
+//  StorageManager.swift
+//  Csere
+//
+//  Created by Athul Sasikumar on 07/09/1943 Saka.
+//
+
+import Foundation
+import FirebaseStorage
+
+final class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    private let storage = Storage.storage().reference()
+    
+    /*
+     /iimages/athul-gmail-com_profile_picture.png
+     */
+    
+    public typealias UploadPictureCompletion = (Result<String, Error>) -> Void
+    
+    /// Upload picture to firebase storage and returns completion with url string to download
+    public func uploadProfilePicture(with data: Data,
+                                     fileName: String,
+                                     completion: @escaping UploadPictureCompletion) {
+        storage.child("images/\(fileName)").putData(data, metadata: nil, completion: { metadata, error in
+            guard error == nil else {
+                // failed
+                print("failed to upload data to firebase for picture")
+                completion(.failure(StorageErrors.failedToUpload))
+                return
+            }
+            
+            self.storage.child("images/\(fileName)").downloadURL(completion: { url, error in
+                guard let url = url else {
+                    print("Failed to get download url")
+                    completion(.failure(StorageErrors.failedToGetDownloadUrl))
+                    return
+                }
+                
+                let urlString = url.absoluteString
+                print("download url returned: \(urlString)")
+                completion(.success(urlString))
+            })
+        })
+    }
+    
+    public enum StorageErrors: Error {
+        case failedToUpload
+        case failedToGetDownloadUrl
+    }
+    public func downloadURL(for path: String, completion:  @escaping (Result<URL, Error>) -> Void) {
+        let reference = storage.child(path)
+        
+        reference.downloadURL(completion: { url, error in
+            guard let url = url, error == nil else {
+            completion(.failure(StorageErrors.failedToGetDownloadUrl))
+            return
+        }
+            completion(.success(url))
+        })
+    }
+}
